158500c6555c4d42df99c8e74b676ded660c1d5c
Mounika <mounikakandula475@gmail.com>
2023-04-06 14:03:57 +0530

    [Mounika] Adding Modified Code

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to 276btach");
+            Console.WriteLine("Code pushing from 2nd Developer");
+            Console.WriteLine("Modifying code from remote repository");
+            Console.WriteLine("Modifying in local repository");
+            Console.ReadLine();
+        }
+    }
+}
+
